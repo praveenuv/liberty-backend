59bbea38aee9f09becf63c170a00f0253d71b549
praveenuv <45493211+praveenuv@users.noreply.github.com>
2024-01-10 00:04:27 +0530

    jar commit

--- src/main/java/liberty/emsbackend/Controller/EmployeeController.java
+++ src/main/java/liberty/emsbackend/Controller/EmployeeController.java
@@ -1,27 +1,26 @@
 package liberty.emsbackend.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import liberty.emsbackend.Dto.EmployeeDto;
 import liberty.emsbackend.Service.EmployeeService;
 import java.io.IOException;
 
-import java.io.IOException;
+
 import java.util.List;
-import org.springframework.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("*")
